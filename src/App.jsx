d1ea8df29662269eb4fa7057e28bcb975216891c
annagrnva <m-ippolitova@mail.ru>
2025-03-01 00:57:05 +0400

    useState practise

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { use, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [text, setText] = useState()
+
+return (
+  <>
+  you enter the text: {text}
+
+  <div>
+    <input 
+    type="text" 
+    value={text}
+    onChange={(event) => setText(event.target.value)}
+    />
+  </div>
+  </>
+)
+
+  //Пример с именами
+// const [name, setName] = useState()
+// const [surname, setSurname] = useState()
+
+// return (
+//   <>
+//   first name: {name} <br/>
+//   last name: {surname} <br/>
+//   <button onClick={() => setName('Anna')}>Anna</button>
+//   <button onClick={() => setSurname('Gurianova')}>Gurianova</button> <br/>
+//   <button onClick={() => setName('Ilya')}>Ilya</button>
+//   <button onClick={() => setSurname('Franx')}>Franx</button>
+//   </>
+// )
+
+  //Пример с цветами
+  // const [color, setColor] = useState('red')
+
+  // return (
+  //   <>
+  //   Color: {color}
+  //   <div 
+  //   className='box'
+  //   style={{
+  //     backgroundColor: color
+  //   }}
+  //   />
+
+  //   <button onClick={() => setColor('blue')}>blue</button>
+  //   <button onClick={() => setColor('green')}>green</button>
+  //   <button onClick={() => setColor('yellow')}>yellow</button>
+
+  //   </>
+  // )
 }
-
-export default App
