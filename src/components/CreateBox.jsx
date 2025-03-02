370a5f32df8f140ee9d0a5a08ddd9c18d8fe5f33
annagrnva <m-ippolitova@mail.ru>
2025-03-03 01:29:26 +0400

    create box component

--- src/components/CreateBox.jsx
+++ src/components/CreateBox.jsx
@@ -4,11 +4,15 @@ import '../App.css'
 export default function CreateBox() {
 
     const [color, setColor] = useState('white')
-    const colorChange = ['red', 'blue', 'yellow', 'green']
-    let counter = 0
+    // const colorChange = ['red', 'blue', 'yellow', 'green']
+    // let counter = 0
 
+    // const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16)
+    // const handleClick = () => {
+    //     setColor(randomColor)
+    // }
     const handleClick = () => {
-        setColor(colorChange[counter])
+        setColor(colorChange + 1)
     };
 
     return (
