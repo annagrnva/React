23a5b3f49c3eac0396adece56ca81f582a6d71c7
annagrnva <m-ippolitova@mail.ru>
2025-03-15 23:39:56 +0400

    handleClick func

--- src/components/CreateBox.jsx
+++ src/components/CreateBox.jsx
@@ -4,15 +4,18 @@ import '../App.css'
 export default function CreateBox() {
 
     const [color, setColor] = useState('white')
-    // const colorChange = ['red', 'blue', 'yellow', 'green']
-    // let counter = 0
+    const colorChange = ['red', 'blue', 'yellow', 'green']
+    let counter = 0
 
     // const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16)
     // const handleClick = () => {
     //     setColor(randomColor)
     // }
     const handleClick = () => {
-        setColor(colorChange + 1)
+       const currentIndex = colorChange.indexOf(color)
+       const nextIndex = (currentIndex + 1) 
+
+       setColor(colorChange[nextIndex])
     };
 
     return (
